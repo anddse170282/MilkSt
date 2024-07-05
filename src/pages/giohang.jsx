300dba15e3b7374f2f3d449a1d5de495629368d6
RikEvolt <146064996+RikEvolt@users.noreply.github.com>
2024-07-05 11:01:10 +0700

    update gio hang

--- src/pages/giohang.jsx
+++ src/pages/giohang.jsx
@@ -61,41 +61,6 @@ const Cart = () => {
   return (
     <>
       <div className="cart-container">
-        <header className="header">
-          <div className="logo-container">
-            <a href="HomePage.html"><img src="https://firebasestorage.googleapis.com/v0/b/imageuploadv3.appspot.com/o/Logo%20Footer%2Fdep2.png?alt=media&token=08b080e1-c883-4c89-ba4f-76f8f0a57b00" width="150" height="150" alt="Logo" />
-            </a>
-          </div>
-          <div className="header-content">
-            <div className="contact-info">
-              <div className="phone"><span>Điện thoại: 0986777514</span></div>
-              <div className="address"><span>Địa Chỉ: D5 .......</span></div>
-              <div className="email"><span>Email: hanafpt@gmail.com</span></div>
-              <a href="#"><img src="taikhoan.jpg" alt="Tài khoản" width="50" height="50" /></a>
-            </div>
-            <div className="header-content-row2">
-              <div className="search-bar-container">
-                <input type="text" placeholder="Tìm kiếm..." />
-                <button type="submit"><img src="search.png" alt="Tìm Kiếm" width="19" height="19" /></button>
-              </div>
-              <div className="user-cart-container">
-                <a href="giohang.html"><img src="giohang.jpg" width="30" height="30" alt="Giỏ hàng" /></a>
-              </div>
-              <div className="notification-container">
-                <a href="#"><img src="bell.png" width="30" height="30" alt="Thông báo" /></a>
-              </div>
-            </div>
-            <nav className="navigation-menu">
-              <ul>
-                <li><a href="HomePage.html">Trang chủ</a></li>
-                <li><a href="#">Sữa bột</a></li>
-                <li><a href="#">Sữa tươi</a></li>
-                <li><a href="#">Sữa chua</a></li>
-                <li><a href="#">Sữa hạt dinh dưỡng</a></li>
-              </ul>
-            </nav>
-          </div>
-        </header>
         <div className='container'>
           <div className='col-md-9'>
             <table className="cart-table">
@@ -128,7 +93,7 @@ const Cart = () => {
                         onChange={(e) => handleItemChange(index, 'selected', e.target.checked)}
                       />
                     </td>
-                    <td><img src="sua.jpg" alt="IMG" width="50" /></td>
+                    <td><img src="" alt="IMG" width="50" /></td>
                     <td>
                       <input
                         type="number"
