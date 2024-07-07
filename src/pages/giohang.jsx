cae12a4a7f2c88fd0ce511eb31de5c4f2d095a51
unknown <laivonguyen@gmail.com>
2024-07-07 17:09:43 +0700

    change how cart get product info

--- src/pages/giohang.jsx
+++ src/pages/giohang.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import '../css/giohang.css';
@@ -9,14 +8,16 @@ const Cart = () => {
   const [subtotal, setSubtotal] = useState(0);
   const [discount, setDiscount] = useState(0);
   const [voucherListVisible, setVoucherListVisible] = useState(false);
-  const [milks, setMilks] = useState([]);
-  const [orders, setOrders] = useState([]);
 
   useEffect(() => {
-    // Fetch cart items from session storage
-    const cart = JSON.parse(sessionStorage.getItem('cart')) || [];
-    setCartItems(cart);
+    // Get cart items from sessionStorage
+    const storedCart = JSON.parse(sessionStorage.getItem('cart')) || [];
+    setCartItems(storedCart);
   }, []);
+
+  useEffect(() => {
+    updateTotals();
+  }, [cartItems]);
 
   const applyDiscount = (discountType) => {
     let discountAmount = 0;
@@ -59,97 +60,85 @@ const Cart = () => {
     updateTotals();
   };
 
-  const getProductById = (id) => {
-    return milks.find(milk => milk.milkId === id);
-  };
-
   const result = subtotal - discount;
 
   return (
-    <>
-      <div className="cart-container">
-        <div className='container'>
-          <div className='col-md-10 cart-table-container'>
-            <table className="cart-table">
-              <thead>
-                <tr>
-                  <th>
+    <div className="cart-container">
+      <div className='container'>
+        <div className='col-md-10 cart-table-container'>
+          <table className="cart-table">
+            <thead>
+              <tr>
+                <th>
+                  <input
+                    type="checkbox"
+                    onChange={(e) => {
+                      const selected = e.target.checked;
+                      const newCartItems = cartItems.map(item => ({ ...item, selected }));
+                      setCartItems(newCartItems);
+                      updateTotals();
+                    }}
+                  />ALL
+                </th>
+                <th>Tên sản phẩm</th>
+                <th>Số lượng</th>
+                <th>Giá tiền</th>
+                <th>Xoá</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cartItems.map((item, index) => (
+                <tr key={index}>
+                  <td>
                     <input
                       type="checkbox"
-                      onChange={(e) => {
-                        const selected = e.target.checked;
-                        const newCartItems = cartItems.map(item => ({ ...item, selected }));
-                        setCartItems(newCartItems);
-                        updateTotals();
-                      }}
-                    />ALL
-                  </th>
-                  <th>Tên sản phẩm</th>
-                  <th>Số lượng</th>
-                  <th>Giá tiền</th>
-                  <th>Xoá</th>
+                      checked={item.selected || false}
+                      onChange={(e) => handleItemChange(index, 'selected', e.target.checked)}
+                    />
+                  </td>
+                  <td>
+                    {item.milkPictures && item.milkPictures.length > 0 && (
+                      <img src={item.milkPictures[0].picture} alt={item.milkName} width="50" />
+                    )}
+                    {item.milkName}
+                  </td>
+                  <td>
+                    <input
+                      type="number"
+                      value={item.quantity}
+                      onChange={(e) => handleItemChange(index, 'quantity', parseInt(e.target.value))}
+                      min="1"
+                    />
+                  </td>
+                  <td>{item.price} ₫</td>
+                  <td>
+                    <button className="delete-btn" onClick={() => handleDeleteItem(index)}>Xoá</button>
+                  </td>
                 </tr>
-              </thead>
-              <tbody>
-                {cartItems.map((item, index) => {
-                  const product = getProductById(item.milkId);
-                  return (
-                    <tr key={index}>
-                      <td>
-                        <input
-                          type="checkbox"
-                          checked={item.selected || false}
-                          onChange={(e) => handleItemChange(index, 'selected', e.target.checked)}
-                        />
-                      </td>
-                      <td>
-                        {product && product.milkPictures && product.milkPictures.length > 0 && (
-                          <img src={product.milkPictures[0].picture} alt={product.milkName} width="50" />
-                        )}
-                        {product ? product.milkName : 'Không xác định'}
-                      </td>
-
-                      <td>
-                        <input
-                          type="number"
-                          value={item.quantity}
-                          onChange={(e) => handleItemChange(index, 'quantity', parseInt(e.target.value))}
-                          min="1"
-                        />
-                      </td>
-                      <td>
-                        {product ? product.price : 'Không xác định'} ₫
-                      </td>
-                      <td>
-                        <button className="delete-btn" onClick={() => handleDeleteItem(index)}>Xoá</button>
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div className="cart-summary col-md-2">
+          <div className='col'><button onClick={toggleVoucherList}>Voucher ưu đãi</button></div>
+          <div className='col'>
+            {voucherListVisible && (
+              <div className="voucher-list">
+                <ul>
+                  <li onClick={() => applyDiscount('Voucher giảm 10%')}>Voucher giảm 10%</li>
+                  <li onClick={() => applyDiscount('Voucher giảm 20.000 ₫')}>Voucher giảm 20.000 ₫</li>
+                  <li onClick={() => applyDiscount('Voucher giảm 50.000 ₫')}>Voucher giảm 50.000 ₫</li>
+                </ul>
+              </div>
+            )}
           </div>
-          <div className="cart-summary col-md-2">
-            <div className='col'><button onClick={toggleVoucherList}>Voucher ưu đãi</button></div>
-            <div className='col'>
-              {voucherListVisible && (
-                <div className="voucher-list">
-                  <ul>
-                    <li onClick={() => applyDiscount('Voucher giảm 10%')}>Voucher giảm 10%</li>
-                    <li onClick={() => applyDiscount('Voucher giảm 20.000 ₫')}>Voucher giảm 20.000 ₫</li>
-                    <li onClick={() => applyDiscount('Voucher giảm 50.000 ₫')}>Voucher giảm 50.000 ₫</li>
-                  </ul>
-                </div>
-              )}
-            </div>
-            <p>Tiền phụ: <span>{subtotal} ₫</span></p>
-            <p>Voucher ưu đãi: <span>{discount} ₫</span></p>
-            <p className="total">Thành Tiền: <span>{result} ₫</span></p>
-            <a href="/pay"><button>Xác nhận giỏ hàng</button></a>
-          </div>
+          <p>Tiền phụ: <span>{subtotal} ₫</span></p>
+          <p>Voucher ưu đãi: <span>{discount} ₫</span></p>
+          <p className="total">Thành Tiền: <span>{result} ₫</span></p>
+          <a href="/pay"><button>Xác nhận giỏ hàng</button></a>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
