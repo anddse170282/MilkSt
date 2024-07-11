66392948a29e69fa5177db422ad55bb266a26852
GiaHeplBro <nguyenduonggia63@gmail.com>
2024-07-11 12:47:07 +0700

    Update Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,47 +7,47 @@ const Header = () => {
   return (
     <header className="header">
       <div className="logo-container">
-          <a href="/">
-            <img src="https://firebasestorage.googleapis.com/v0/b/imageuploadv3.appspot.com/o/Logo%20Footer%2Fdep2.png?alt=media&token=08b080e1-c883-4c89-ba4f-76f8f0a57b00" width="150" height="150" alt="Logo" />
+        <a href="/">
+          <img src="https://firebasestorage.googleapis.com/v0/b/imageuploadv3.appspot.com/o/Logo%20Footer%2Fdep2.png?alt=media&token=08b080e1-c883-4c89-ba4f-76f8f0a57b00" width="150" height="150" alt="Logo" />
+        </a>
+      </div>
+      <div className="header-content">
+        <div className="contact-info">
+          <div className="phone"><span>Mua hàng và CSKH: <span className="phone-color">0986777514</span></span></div>
+          <div className="cam-on"><span>Cảm ơn ba mẹ đã tin dùng HanaStore</span></div>
+          <div className="email"><span>Email: <span className="email-color">nhanltse170178@fpt.edu.vn</span></span></div>
+          <a href="/login">
+            <img src="taikhoan.jpg" alt="Tài khoản" width="50" height="50" />
           </a>
         </div>
-        <div className="header-content">
-          <div className="contact-info">
-            <div className="phone"><span>Điện thoại: 0986777514</span></div>
-            <div className="address"><span>Địa Chỉ: D5 .......</span></div>
-            <div className="email"><span>Email: hanafpt@gmail.com</span></div>
-            <a href="/login">
-              <img src="taikhoan.jpg" alt="Tài khoản" width="50" height="50" />
+        <div className="header-content-row2">
+          <div className="search-bar-container">
+            <input type="text" placeholder="Tìm kiếm..." />
+            <button type="submit">
+              <i className="bi-search icon-small"></i>
+            </button>
+          </div>
+          <div className="user-cart-container">
+            <a href="/cart">
+              <i className="bi bi-cart icon-small"></i>
             </a>
           </div>
-          <div className="header-content-row2">
-            <div className="search-bar-container">
-              <input type="text" placeholder="Tìm kiếm..." />
-              <button type="submit">
-              <i className="bi-search icon-small"></i>
-              </button>
-            </div>
-            <div className="user-cart-container">
-              <a href="/cart">
-              <i className="bi-cart-fill icon-small"></i>
-              </a>
-            </div>
-            <div className="notification-container">
-              <a href="#">
-              <i className="bi-bell-fill icon-small"></i>
-              </a>
-            </div>
+          <div className="notification-container">
+            <a href="#">
+              <i className="bi bi-bell icon-small"></i>
+            </a>
           </div>
-          <nav className="navigation-menu">
-            <ul>
-              <li><a href="/">Trang chủ</a></li>
-              <li><a href="/">Sữa bột</a></li>
-              <li><a href="/">Sữa tươi</a></li>
-              <li><a href="/">Sữa chua</a></li>
-              <li><a href="/">Sữa hạt dinh dưỡng</a></li>
-            </ul>
-          </nav>
         </div>
+        <nav className="navigation-menu">
+          <ul>
+            <li><a href="/">Trang chủ</a></li>
+            <li><a href="/">Sữa bột</a></li>
+            <li><a href="/">Sữa tươi</a></li>
+            <li><a href="/">Sữa chua</a></li>
+            <li><a href="/">Sữa hạt dinh dưỡng</a></li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
